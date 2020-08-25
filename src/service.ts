eb3fc963a5519d9e43c075eca0bae10ad098f7cd
Guilherme Sartori <gui.sartori96@gmail.com>
2020-08-25 13:14:06 -0300

    teste

--- src/service.ts
+++ src/service.ts
@@ -1,8 +1,8 @@
-import Axios from 'axios'
+import Axios from "axios";
 
 const api = Axios.create({
-    baseURL: "http://localhost:3333/",
-    timeout: 1000
-})
+    baseURL: "https://call-manager-api.herokuapp.com/",
+    timeout: 1000,
+});
 
-export default api
+export default api;
